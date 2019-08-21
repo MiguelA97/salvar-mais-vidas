d92926d7b58e7b7ee94ce01a0e6bba92087aead5
MiguelA97 <53756806+MiguelAchegaAlter@users.noreply.github.com>
2019-08-21 11:26:41 +0100

    Fixed directorys

---
+++ src/test/java/com/salvarmaisvidas/SalvarMaisVidasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salvarmaisvidas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalvarMaisVidasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
