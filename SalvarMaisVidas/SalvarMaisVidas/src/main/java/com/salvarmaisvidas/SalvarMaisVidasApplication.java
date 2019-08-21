b0a3797318279ecfb2e7f7aedfa9392f6926ea25
MiguelA97 <43322994+MiguelA97@users.noreply.github.com>
2019-08-21 10:49:11 +0100

    Mariadb dependency added

---
+++ SalvarMaisVidas/SalvarMaisVidas/src/main/java/com/salvarmaisvidas/SalvarMaisVidasApplication.java
@@ -0,0 +1,13 @@
+package com.salvarmaisvidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalvarMaisVidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvarMaisVidasApplication.class, args);
+	}
+
+}
