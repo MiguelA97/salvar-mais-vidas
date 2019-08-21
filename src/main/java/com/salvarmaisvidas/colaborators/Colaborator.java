56a309288666bfb904422bfc3b1c818e4a8e1fa1
MiguelA97 <53756806+MiguelAchegaAlter@users.noreply.github.com>
2019-08-21 15:15:34 +0100

    Colaborator class done

--- src/main/java/com/salvarmaisvidas/colaborators/Colaborator.java
+++ src/main/java/com/salvarmaisvidas/colaborators/Colaborator.java
@@ -1,4 +1,126 @@
 package com.salvarmaisvidas.colaborators;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@IdClass(ColaboratorId.class)
 public class Colaborator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Id
+    private String name;
+
+    private String email;
+    private LocalDate birthday;
+    private int cc;
+    private String address;
+    private String postal_code;
+    private String locality;
+    private int phone;
+    private String job;
+    private boolean trainer;
+    private LocalDate registration_date;
+
+    public Colaborator(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public boolean isTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(boolean trainer) {
+        this.trainer = trainer;
+    }
+
+    public LocalDate getRegistration_date() {
+        return registration_date;
+    }
+
+    public void setRegistration_date(LocalDate registration_date) {
+        this.registration_date = registration_date;
+    }
 }
