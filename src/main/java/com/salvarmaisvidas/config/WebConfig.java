4838427c8f319a087f566ff8794030202cc36ec4
MiguelA97 <53756806+MiguelAchegaAlter@users.noreply.github.com>
2019-08-22 10:27:46 +0100

    Web Config added

---
+++ src/main/java/com/salvarmaisvidas/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.salvarmaisvidas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Allow Cross-origin HTTP Requests
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // TODO: This allows angular to work with remote servers, should not be in a production environment
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "GET", "POST");
+    }
+
+    /**
+     * Configure the Angular folder to be available outside of the jar.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/") // path to the frontend build
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : location.createRelative("index.html");
+                    }
+                });
+
+    }
+
+}
